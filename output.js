6ed80fac32bb9ccbf10905e774f24b0a9f3dc70a
action <action@github.com>
2025-03-08 12:17:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Mar 06 2025 07:41:56 GMT+0000 (Coordinated Universal Time)
+//Sat Mar 08 2025 12:17:19 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("甬派");
